08350718d2cb9bd148066d41fa8867c7b2d01091
Fahim Tajwar <oscorp4@gmail..com>
2022-07-05 22:04:50 +0600

    Added Statefulness to Search.

--- pages/SearchPage.js
+++ pages/SearchPage.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, StyleSheet, TextInput, useWindowDimensions} from "react-native";
 import {LargeButton} from "../components/LargeButton";
 
 const SearchPage = () => {
     // const styles = makestyle(useWindowDimensions().fontScale)
+    const [searchTerm, setSearchTerm] = useState();
+
+    const searchHandler = (query) => {
+        setSearchTerm(query);
+    }
     return (
         <View style = {styles.mainContainer}>
             <TextInput  style = {[styles.searchBar, styles.inputText]}
                         placeholder = {"Search for a book here"}
+                        onChangeText = {searchHandler}
             />
-            <LargeButton buttonName = {"Search"} onPress = {() => console.log("Searched")}/>
+            <LargeButton 
+                        buttonName = {"Search"} 
+                        onPress = {() => console.log(searchTerm)} //Pass to Google Books API
+                        style = {styles.searchButton}
+                    />
         </View>
     )
 }
@@ -18,11 +28,12 @@ const styles = StyleSheet.create({
     mainContainer: {
         alignItems: "center",
         backgroundColor: "#dbb49c",
+        flexDirection: "column",
         flex: 1
     },
     searchBar: {
         backgroundColor: "#ebebeb",
-        flex: 0.12,
+        flex: 0.1,
         width: "80%",
         borderWidth: 1.5,
         paddingLeft: "4%",
@@ -32,6 +43,9 @@ const styles = StyleSheet.create({
     },
     inputText: {
         fontSize: 19
+    },
+    searchButton: {
+        flex: 0.07,
     }
 });
 
